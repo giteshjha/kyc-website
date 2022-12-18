f5f618de5572eba4157adbdb55d998717e7002a9
Shubham Jha <Shubhamkumarjha0013@gmail.com>
2022-12-18 19:05:32 +0530

    Update EventData.ts

--- components/Main/EventData.ts
+++ components/Main/EventData.ts
@@ -2,6 +2,10 @@ export const EventData = [
     {
       image:
         "https://ik.imagekit.io/kyc/events/1.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1671208260823",
+    },
+    {
+      image:
+        "https://ik.imagekit.io/kyc/events/2.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1671370447120",
     }
   ];
   
